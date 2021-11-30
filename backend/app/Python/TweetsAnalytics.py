4e81a9d86a8c2cd4d0db07af6b7ce2931a70ba65
Shuto Sato <age.sugar.0924@gmail.com>
2021-11-30 23:59:17 +0900

    modified python file

--- backend/app/Python/TweetsAnalytics.py
+++ backend/app/Python/TweetsAnalytics.py
@@ -42,8 +42,8 @@ for tweet in api.search_tweets(q=searchCommand, lang='ja', result_type='recent',
 # ツイート数の入れ直し
 NumberOfAllTweets = len(Tweets)
 # osetiで解析をかける
-osetiResult = map(analyzer.analyze, TweetsAnalytics)
-negaPosiResults = list(osetiResult)
+# negaPosiResults = list(map(analyzer.analyze, TweetsAnalytics))
+negaPosiResults = [analyzer.analyze(i) for i in TweetsAnalytics]
 # 変数定義
 NumberOfPositiveTweets = 0 # positiveツイートの数
 NumberOfNeutralTweets = 0 # neutralツイートの数
