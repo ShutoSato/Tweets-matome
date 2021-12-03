8e48a18f9284b6f16eb83570845ab973926d30aa
Shuto Sato <age.sugar.0924@gmail.com>
2021-12-03 15:33:42 +0900

    edited pythonfile

--- backend/app/Python/TweetsAnalytics.py
+++ backend/app/Python/TweetsAnalytics.py
@@ -24,7 +24,9 @@ api = tweepy.API(auth)
 tokenizer = MeCab.Tagger('-r /dev/null -d /app/.heroku/python/lib/python3.6/site-packages/unidic_lite/dicdir')
 tokenizer.parse("")
 #osetiの準備
-analyzer = oseti.Analyzer('mecab_args=ipadic.MECAB_ARGS')
+analyzer = oseti.Analyzer(mecab_args=ipadic.MECAB_ARGS)
+test = analyzer.analyze('天国で待ってる。')
+print(test)
 #配列準備
 Tweets = [] # ツイートを格納する配列
 TweetsAnalytics = [] # 解析する用のツイートを格納する配列
