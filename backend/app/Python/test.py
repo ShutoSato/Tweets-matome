eb2f5ccd840e472fac2f03d996a5345f6590075a
Shuto Sato <age.sugar.0924@gmail.com>
2021-12-03 21:09:52 +0900

    test

--- backend/app/Python/test.py
+++ backend/app/Python/test.py
@@ -1,5 +1,5 @@
 import oseti
 import ipadic
 
-analyzer = oseti.Analyzer('-r /app/.heroku/python/lib/python3.6/site-packages/ipadic/dicdir/mecabrc')
+analyzer = oseti.Analyzer(mecab_args=ipadic.MECAB_ARGS)
 analyzer.analyze('天国で待ってる。')
