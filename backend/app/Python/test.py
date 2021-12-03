3b616d18ca936d1743804c8138d3059c62e2d18f
Shuto Sato <age.sugar.0924@gmail.com>
2021-12-03 20:57:18 +0900

    test

--- backend/app/Python/test.py
+++ backend/app/Python/test.py
@@ -1,5 +1,5 @@
 import oseti
 import ipadic
 
-analyzer = oseti.Analyzer('mecab_args=ipadic.MECAB_ARGS -r /app/.heroku/python/lib/python3.6/site-packages/ipadic/dicdir/mecabrc')
+analyzer = oseti.Analyzer('-r /app/.heroku/python/lib/python3.6/site-packages/ipadic/dicdir/mecabrc')
 analyzer.analyze('天国で待ってる。')
